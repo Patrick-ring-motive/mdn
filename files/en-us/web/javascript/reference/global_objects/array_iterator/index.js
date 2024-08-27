701b3373613fb25250d7c902f102f957905e6450
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-27 14:55:05 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -1,1 +1,8 @@
+void async function ArrayIterators(){
 
+  globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
+  declare(()=>{
+    swapText('Iterator', 'Array Iterator');
+  });
+  
+}?.();
