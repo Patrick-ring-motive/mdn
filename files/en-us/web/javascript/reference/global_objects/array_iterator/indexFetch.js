91da5f5db9e397337dbc650a330edfc3ad15ecce
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-27 14:31:24 -0500

    Update indexFetch.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/indexFetch.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/indexFetch.js
@@ -1,1 +1,19 @@
+const objDoProp = function (obj, prop, def, enm, mut) {
+  return Object.defineProperty(obj, prop, {
+    value: def,
+    writable: mut,
+    enumerable: enm,
+    configurable: mut
+  });
+};
+const objDefProp=(obj, prop, def) => objDoProp (obj, prop, def, false, true);
 
+if(!globalThis['&fetch']){
+  objDefProp(globalThis,"&fetch",fetch);
+  globalThis.fetch = function fetch(url){
+    if(~String(arguments[0]).search(/index\.json($|\?.*|#.*)/i)){
+      arguments[0] = `${document.querySelector('link[hreflang="en"]')?.getAttribute?.('href')}/index.json`;
+    }
+    return globalThis['&fetch'].call(this,...arguments);
+  }
+}
