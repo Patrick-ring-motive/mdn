1f3f0d57b7456babe1a8a44698e188d11e7aef2f
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-28 11:51:18 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -7,12 +7,12 @@ void async function ArrayIterators(){
   await DOMComplete();
   swapTextBack('Iterator', 'Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
-  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1]));
+  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + `<link rel="stylesheet" href="https://patrick-ring-motive.github.io/mdn/assets/css/style.css">`;
   Q()=>(select('article>div.section-content').innerHTML = content));
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
       el.setAttribute('written',true);
-      el.innerHTML = content + `<link rel="stylesheet" href="https://patrick-ring-motive.github.io/mdn/assets/css/style.css">` ;
+      el.innerHTML = content;
     });
   });
 }?.();
