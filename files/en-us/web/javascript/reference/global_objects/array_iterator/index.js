5c835a4a04ed67808ca63dddc41e2b5f276f16c3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-28 11:59:36 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -9,7 +9,9 @@ void async function ArrayIterators(){
   swapTextBack('Iterator', 'Array Iterator');
   document.title = document.title.replace('Iterator','Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
-  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + `<link rel="stylesheet" href="https://patrick-ring-motive.github.io/mdn/assets/css/style.css">`;
+  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
+`<script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>
+ <link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
   Q(()=>(select('article>div.section-content').innerHTML = content));
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
