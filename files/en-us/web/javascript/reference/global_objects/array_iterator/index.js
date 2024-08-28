766d7ac9aab471af5607d739adabc1aa45e270bc
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-28 13:32:19 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -53,5 +53,5 @@ void async function ArrayIterators(){
   let prism = document.createElement('script');
   prism.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js';
   document.body.appendChild(prism);
-  queryApplyAll('.highlighter-rouge:not(.language-js)',el=>el.setAttribute('class','');
+  queryApplyAll('.highlighter-rouge:not(.language-js)',el=>el.setAttribute('class',''));
 }?.();
