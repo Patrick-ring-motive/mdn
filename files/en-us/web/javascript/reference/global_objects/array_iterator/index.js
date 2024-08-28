9681a4ff64e43007d6201cf793ca332efd465bbe
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-28 12:25:43 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -11,9 +11,10 @@ void async function ArrayIterators(){
   swapTextBack('Iterator', 'Array Iterator');
   document.title = document.title.replace('Iterator','Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
-  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
-`<script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>
- <link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
+  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + `<link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
+  let prism = document.createElement('script');
+  prism.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js';
+  document.body.appendChild(prism);
   Q(()=>(select('article>div.section-content').innerHTML = content));
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
