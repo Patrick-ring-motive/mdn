c1694a46d92203c57ee068941bd245c2efe43deb
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-28 11:53:30 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -10,7 +10,7 @@ void async function ArrayIterators(){
   document.title = document.title.replace('Iterator','Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
   const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + `<link rel="stylesheet" href="https://patrick-ring-motive.github.io/mdn/assets/css/style.css">`;
-  Q()=>(select('article>div.section-content').innerHTML = content));
+  Q(()=>(select('article>div.section-content').innerHTML = content));
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
       el.setAttribute('written',true);
