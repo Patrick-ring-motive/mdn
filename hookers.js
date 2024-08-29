beb7958f8f10045f3784349c392f734c8f8ec2b7
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-29 09:09:14 -0500

    Update hookers.js

--- hookers.js
+++ hookers.js
@@ -3,6 +3,9 @@ void async function ArrayIterators(){
   globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
   declare(()=>{
     swapTextBack('MDN', 'TDN');
+    swapTextBack('mdn', 'tdn');
+    swapTextBack('Joshua Chen', 'John Cena');
+    swapTextBack('TDN is the most comprehensive, up-to-date, and professional documentation about the web. This is all thanks to its diverse writer team as well as a strong contributor community, who have consistently demonstrated expertise in every area.', "You Can't See Me");
   });
   
 }?.();
