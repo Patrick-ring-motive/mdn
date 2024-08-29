2c70392bd4092ea7f964387712de74d278c494db
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-29 09:50:18 -0500

    Update hookers.js

--- hookers.js
+++ hookers.js
@@ -7,5 +7,22 @@ void async function ArrayIterators(){
     swapTextBack('Joshua Chen', 'John Cena');
     swapTextBack('TDN is the most comprehensive, up-to-date, and professional documentation about the web. This is all thanks to its diverse writer team as well as a strong contributor community, who have consistently demonstrated expertise in every area.', "You Can't See Me");
   });
+
+  declare(()=>{
+    const bodyStyles = window.getComputedStyle(document.body);
+    const b = bodyStyles.getPropertyValue("background-color").replace(/[^0-9,]/g,'').split(",")
+    if(b.length == 3 && b.every(x=>x<128)){
+      document.body.updateAttribute('darkish',true);
+    }else{
+      document.body.updateAttribute('darkish',false);
+    }
+    const htmlStyles = window.getComputedStyle(page_html);
+    const h = htmlStyles.getPropertyValue("background-color").replace(/[^0-9,]/g,'').split(",")
+    if(h.length == 3 && h.every(x=>x<128)){
+      page_html.updateAttribute('darkish',true);
+    }else{
+      page_html.updateAttribute('darkish',false);
+    }
+  });
   
 }?.();
