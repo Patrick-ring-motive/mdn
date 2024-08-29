3e5e56f747042f945723f642b15b202d053a369c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-29 14:11:54 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -40,8 +40,18 @@ void async function ArrayIterators(){
   let prism = document.createElement('script');
   prism.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js';
   document.body.appendChild(prism);
+  queryApplyAll('.highlighter-rouge:not(.language-js):not([class="0"])',el=>el.setAttribute('class','0'));
   declare(()=>{
     queryApplyAll('.highlighter-rouge:not(.language-js):not([class="0"])',el=>el.setAttribute('class','0'));
+  });
+  declare(()=>{
+    queryApplyAll('.language-js',el=>{
+      if(el.clientWidth > window.clientWidth()){
+        el.updateAttribute('shrink-me',true);
+      }else{
+        el.updateAttribute('shrink-me',false);
+      }
+    });
   });
   queryApplyAll('blockquote:not([class])',el=>el.setAttribute('class','notecard note'));
   await waitExists('aside.toc .document-toc-item:has([href="#description"])');
