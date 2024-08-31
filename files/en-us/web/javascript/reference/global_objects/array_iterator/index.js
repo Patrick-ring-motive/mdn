8698485b57941c46ee3363f4963624ddeb7808a2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-30 22:44:09 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -10,12 +10,13 @@ void async function ArrayIterators(){
   //swapTextBack('Iterator', 'Array Iterator');
   await DOMInteractive();
   swapTextBack('Iterator', 'Array Iterator');
-  if(!~document.title.search(/Array/)){document.title = document.title.replace(/^Iterator/,'Array Iterator');}
+  if(!~`${document.title}`.search(/Array/)){
+    document.title = document.title.replace(/^Iterator/,'Array Iterator');
+  }
   console.log(document.readyState);
   await DOMComplete();
   console.log('DOMComplete');
   swapTextBack('Iterator', 'Array Iterator');
-  document.title = document.title.replace('Iterator','Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
   const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
     `<link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
@@ -25,7 +26,9 @@ void async function ArrayIterators(){
   });
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
-      if(!~document.title.search(/Array/)){document.title = document.title.replace(/^Iterator/,'Array Iterator');}
+      if(!~`${document.title}`.search(/Array/)){
+        document.title = document.title.replace(/^Iterator/,'Array Iterator');
+      }
       el.setAttribute('written',true);
       swapTextBack('Iterator', 'Array Iterator');
       el.innerHTML = content;
