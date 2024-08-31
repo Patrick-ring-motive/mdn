11a7df372c1d167363342acdd8d198109e148cf1
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-08-31 14:51:59 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -9,14 +9,14 @@ void async function ArrayIterators(){
   swapTitle();
   //swapTextBack('Iterator', 'Array Iterator');
   await DOMInteractive();
-  swapTextBack('Iterator', 'Array Iterator');
+  //swapTextBack('Iterator', 'Array Iterator');
   if(!~`${document.title}`.search(/Array/)){
     document.title = document.title.replace(/^Iterator/,'Array Iterator');
   }
   console.log(document.readyState);
   await DOMComplete();
   console.log('DOMComplete');
-  swapTextBack('Iterator', 'Array Iterator');
+  //swapTextBack('Iterator', 'Array Iterator');
   const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
   const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
     `<link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
@@ -30,7 +30,7 @@ void async function ArrayIterators(){
         document.title = document.title.replace(/^Iterator/,'Array Iterator');
       }
       el.setAttribute('written',true);
-      swapTextBack('Iterator', 'Array Iterator');
+      //swapTextBack('Iterator', 'Array Iterator');
       el.innerHTML = content;
       let prism = document.createElement('script');
       prism.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js';
