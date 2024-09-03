43a9320da7ab185d04a5b54202fddc049e34be1e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-03 18:58:48 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -22,7 +22,17 @@ void async function ArrayIterators(){
   if(!~`${document.title}`.search(/Array/)){
     document.title = document.title.replace(/^Iterator/,'Array Iterator');
   }
-
+  declare(()=>{
+      queryApplyAll('html[window-location]:not([window-location*="hostname=nodejs.org"]) .language-js:not([js-highlighter])',el=>{
+      el.updateAttribute('js-highlighter','js-highlighter');
+      let prism = document.createElement("script");
+      prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+      body().appendChild(prism);
+    });
+    queryApplyAll('html[window-location]:not([window-location*="hostname=nodejs.org"]) .highlighter-rouge:not(.language-js):not([class="0"])', (el) =>
+      el.setAttribute("class", "0"),
+    );
+  });
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
       if(!~`${document.title}`.search(/Array/)){
