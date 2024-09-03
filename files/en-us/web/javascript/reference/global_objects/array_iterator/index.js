ef530fbf586a8e9b86e1f5bba88d8d8727ca71e5
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-03 13:44:55 -0500

    Update index.js

--- files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
+++ files/en-us/web/javascript/reference/global_objects/array_iterator/index.js
@@ -22,17 +22,7 @@ void async function ArrayIterators(){
   if(!~`${document.title}`.search(/Array/)){
     document.title = document.title.replace(/^Iterator/,'Array Iterator');
   }
-  console.log(document.readyState);
-  await DOMComplete();
-  swapTitle();
-  console.log('DOMComplete');
-  const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
-  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
-    `<link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
-  Q(()=>{
-    select('article>div.section-content').innerHTML = content;
-    select('article>div.section-content').setAttribute('written',true);
-  });
+
   declare(()=>{
     queryApplyAll('article>div.section-content:not([written])',el=>{
       if(!~`${document.title}`.search(/Array/)){
@@ -52,7 +42,17 @@ void async function ArrayIterators(){
       el.innerHTML = el.innerHTML.replace('Array Iterator','Array&nbsp;Itеrator');/*Usage of Cyrillic Ye*/;
     });
   });
-
+  console.log(document.readyState);
+  await DOMComplete();
+  swapTitle();
+  console.log('DOMComplete');
+  const resources = Q(()=>JSON.parse(select('script[type="resources"]').innerHTML))??[];
+  const content = decodeURIComponent(atob(resources.find(x=>~x[0].search(/content$/))[1])) + 
+    `<link  href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css" rel="stylesheet"></link>`;
+  Q(()=>{
+    select('article>div.section-content').innerHTML = content;
+    select('article>div.section-content').setAttribute('written',true);
+  });
   queryApplyAll('blockquote:not([class])',el=>el.setAttribute('class','notecard note'));
   /*await waitExists('aside.toc .document-toc-item:has([href="#description"])');
   select('aside.toc .document-toc-item:has([href$="#description"])').insertAdjacentElement("afterend",select('aside.toc .document-toc-item:has([href$="#examples"])'));
