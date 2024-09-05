053baa4ee6b3e7e9ac421760fafc8a087538980e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-05 15:02:21 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -10,6 +10,23 @@ void async function OptionalChaining(){
   await waitExists('section[aria-labelledby="optional_chaining_with_function_calls"]');
   select('section[aria-labelledby="optional_chaining_with_function_calls"]').appendChild(div);
   console.log('asdf')
+      await Promise.all([import(https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify.js),
+
+                                                                                   import(https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify-css.js),
+
+                                                                                   import(https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify-html.js),
+
+                                                                                   import(https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js)]);
+
+                const pretty = function pretty(txt){
+
+                                const options = { indent_size: 2, space_in_empty_paren: true }
+
+                                return js_beautify(txt, options)
+
+                }
+
+
   let prism = createElement("script");
   prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
   body().appendChild(prism);
