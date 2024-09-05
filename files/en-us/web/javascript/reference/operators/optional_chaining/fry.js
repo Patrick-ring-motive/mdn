81658a49a219cb91ab456d89252c0f9551093981
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-05 13:36:27 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -5,10 +5,13 @@ void async function OptionalChaining(){
     await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${new Date().getTime()}`);
   }
   const opFunc = await zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${new Date().getTime()}`);
-  console.log(opFunc);
   const div = createElement('div');
   div.innerHTML = opFunc;
   select('section[aria-labelledby="optional_chaining_with_function_calls"]').appendChild(div);
+
+  let prism = createElement("script");
+  prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+  body().appendChild(prism);
 }?.()
 
 
