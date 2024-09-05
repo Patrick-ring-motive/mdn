941b9fde7d156195afa6779dc73cbfd5ad4ccb16
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-05 13:24:46 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -1,1 +1,3 @@
- 
+
+
+zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main//files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${new Date().getTime()}`)
