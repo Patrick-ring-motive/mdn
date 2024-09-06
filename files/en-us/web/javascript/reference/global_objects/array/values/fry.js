8fd825bee25aa724d373b2348c46c96e8c945d4d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-06 15:02:03 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/global_objects/array/values/fry.js
+++ files/en-us/web/javascript/reference/global_objects/array/values/fry.js
@@ -1,1 +1,13 @@
 /* TODO array iterator links. */
+void async function ArrayIterators(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${new Date().getTime()}`);
+  }
+  declare(() =&gt; {
+    queryApplyAll('a[href="/en-US/docs/Web/JavaScript/Guide/Iterators_and_generators"]',el =&gt; {
+      el.updateAttribute('href','/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array_Iterator');
+    });
+  });
+}?.()
