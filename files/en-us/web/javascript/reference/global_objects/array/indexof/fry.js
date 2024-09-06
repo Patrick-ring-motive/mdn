3a1ccead6ac2a2a12fb25996a45e1a4507e6c4f8
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-06 13:57:39 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/global_objects/array/indexof/fry.js
+++ files/en-us/web/javascript/reference/global_objects/array/indexof/fry.js
@@ -1,1 +1,17 @@
-/* TODO bit shift index trick */
+void async function OptionalChaining(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${new Date().getTime()}`);
+  }
+  
+  const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/global_objects/array/indexof/shiftbit.html?${new Date().getTime()}`);
+  const divOp = createElement('div');
+  divOp.innerHTML = await opFunc;
+  await waitExists('section[aria-labelledby="using_indexof"]');
+  select('section[aria-labelledby="using_indexof"]').appendChild(divOp);
+  
+  const prism = createElement("script");
+  prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+  body().appendChild(prism);
+}?.()
