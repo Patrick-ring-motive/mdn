6edf5f86aab9566d9ad9b8bd5949d18da80a4adf
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-06 14:55:02 -0500

    Update hookers.js

--- hookers.js
+++ hookers.js
@@ -104,7 +104,11 @@ void (async function Hookers() {
     });
   });
   await DOMComplete();
-  let prism = createElement("script");
-  prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
-  body().appendChild(prism);
+  if(Prism){
+    Prism?.highlightAll?.();
+  }else{
+    const prism = createElement("script");
+    prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+    body().appendChild(prism);
+  }
 })?.();
