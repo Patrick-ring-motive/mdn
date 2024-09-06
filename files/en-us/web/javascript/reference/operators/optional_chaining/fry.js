7d042ed1b8fdad5ecf67e212e6896305c8c2e352
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-05 21:05:17 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -6,14 +6,21 @@ void async function OptionalChaining(){
   }
   const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${new Date().getTime()}`);
   const opAss =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-assignments.html?${new Date().getTime()}`);
+  const opShort =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/short-circuit.html?${new Date().getTime()}`);
   const divOp = createElement('div');
   const divAss = createElement('div');
+  const divShort = createElement('div');
   divOp.innerHTML = await opFunc;
   divAss.innerHTML = await opAss;
+  divShort.innerHTML = await opShort;
   await waitExists('section[aria-labelledby="optional_chaining_with_function_calls"]');
   select('section[aria-labelledby="optional_chaining_with_function_calls"]').appendChild(divOp);
   await waitExists('section[aria-labelledby="invalid_optional_chaining"]');
   select('section[aria-labelledby="invalid_optional_chaining"]').appendChild(divAss);
+  await waitExists('section[aria-labelledby="short_circuiting"]');
+  select('section[aria-labelledby="short_circuiting"]').appendChild(divShort);
+  
+  
   /*await Promise.all([import('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify.js'),
                      import('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify-css.js'),
                      import('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify-html.js')]);
