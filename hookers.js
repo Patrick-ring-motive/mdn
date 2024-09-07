97aed2282fc379d42ea0543dc2a940a8df096a64
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-07 08:17:47 -0500

    Update hookers.js

--- hookers.js
+++ hookers.js
@@ -22,7 +22,7 @@ void (async function Hookers() {
       "TDN is the most comprehensive, up-to-date, and professional documentation about the web. This is all thanks to its diverse writer team as well as a strong contributor community, who have consistently demonstrated expertise in every area.",
       "You Can't See Me",
     );
-    swapTextBack(', and JavaScript, since 2005.',', JavaScript, and more.)
+    swapTextBack(', and JavaScript, since 2005.',', JavaScript, and more.')
   });
 
   declare(() => {
