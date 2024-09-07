57f8d88be0f29cbed4f4800634936fc21b0cc521
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-07 08:22:14 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -1,12 +1,12 @@
 void async function OptionalChaining(){
   try{
-    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${globalThis.cache}`);
   }catch{
-    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${new Date().getTime()}`);
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
   }
-  const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${new Date().getTime()}`);
-  const opAss =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-assignments.html?${new Date().getTime()}`);
-  const opShort =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/short-circuit.html?${new Date().getTime()}`);
+  const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${globalThis.cache}`);
+  const opAss =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-assignments.html?${globalThis.cache}`);
+  const opShort =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/short-circuit.html?${globalThis.cache}`);
   const divOp = createElement('div');
   const divAss = createElement('div');
   const divShort = createElement('div');
