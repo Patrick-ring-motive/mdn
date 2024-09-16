dff3fc56853662ca02428de536e317c3fae5989c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-09-16 14:47:17 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/global_objects/array/findindex/fry.js
+++ files/en-us/web/javascript/reference/global_objects/array/findindex/fry.js
@@ -1,1 +1,21 @@
-/* TODO bit shift index trick */
+void async function ArrayFindIndex(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${globalThis.cache}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
+  }
+  
+  const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/global_objects/array/indexof/shiftbit.html?${globalThis.cache}`);
+  const divOp = createElement('div');
+  divOp.innerHTML = await opFunc;
+  await waitExists('section[aria-labelledby="find_the_index_of_a_prime_number_in_an_array"]');
+  select('section[aria-labelledby="using_indexof"]').appendChild(divOp);
+  
+  try {
+      Prism?.highlightAll?.();
+  } catch {
+      const prism = createElement("script");
+      prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+      body().appendChild(prism);
+  }
+}?.()
