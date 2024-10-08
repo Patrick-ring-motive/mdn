47ed0877fefd038175cdfaf11e179d56af850ade
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-08 13:02:34 -0500

    Create fry.js

---
+++ files/en-us/web/api/readablestream/readablestream/fry.js
@@ -0,0 +1,20 @@
+void async function ReadableStreamApi(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${globalThis.cache}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
+  }
+
+  const responseBody = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/api/readablestream/readablestream/response-body.html?${globalThis.cache}`);
+  (await waitSelect('section[aria-labelledby="examples"]')).appendChild(assign(createElement('div'),{
+    innerHTML: (await responseBody)
+  }));
+
+  try {
+      Prism?.highlightAll?.();
+  } catch {
+      const prism = createElement("script");
+      prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+      body().appendChild(prism);
+  }
+}?.()
