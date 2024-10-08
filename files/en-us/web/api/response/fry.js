0f8a7fd4c27e1c7abec7fab5280a81982cfbfc66
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-08 15:21:33 -0500

    Create fry.js

---
+++ files/en-us/web/api/response/fry.js
@@ -0,0 +1,28 @@
+void async function ResponseApi(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${globalThis.cache}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
+  }
+  const code1 = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/api/window/fetch/example1.html?${globalThis.cache}`);
+  const fetchTextExample = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/api/window/fetch/fetch-text.html?${globalThis.cache}`);
+  await waitExists('section[aria-labelledby="examples"]');
+
+  
+  async function replaceCode(exampleDiv,code){
+    exampleDiv.before(assign(createElement('div'),{innerHTML:(await code)}));
+    exampleDiv.style.display = 'none';
+    exampleDiv.updateAttribute('overwritten',true);
+  }
+
+  await replaceCode(select('section[aria-labelledby="examples"] .code-example:not([overwritten])'),code1);
+  select('section[aria-labelledby="examples"]').appendChild(assign(createElement('div'),{innerHTML:(await fetchTextExample)}));
+  
+  try {
+      Prism?.highlightAll?.();
+  } catch {
+      const prism = createElement("script");
+      prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+      body().appendChild(prism);
+  }
+}?.()
