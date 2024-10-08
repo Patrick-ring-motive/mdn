f82f57acb81ec7d300b3b31e493ff5ef38f0b9e5
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-07 22:29:10 -0500

    Update fry.js

--- files/en-us/web/api/window/fetch/fry.js
+++ files/en-us/web/api/window/fetch/fry.js
@@ -9,7 +9,7 @@ void async function OptionalChaining(){
   const example1 = createElement('div');
   example1.innerHTML = await code1;
   await waitExists('section[aria-labelledby="examples"]');
-  const exampleDiv = select('section[aria-labelledby="examples"]').querySelectorAll('pre,code')[1];
+  const exampleDiv = select('section[aria-labelledby="examples"]').querySelectorAll('pre,code')[2];
   exampleDiv.before(example1);
   exampleDiv.style.display = 'none';
   
