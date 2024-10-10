609857d767a273ef325c744e579072bd1d8ebd8d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-10 10:54:57 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -5,15 +5,19 @@ void async function OptionalChaining(){
     await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
   }
   const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${globalThis.cache}`);
+  const opNew =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-new.html?${globalThis.cache}`);
   const opAss =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-assignments.html?${globalThis.cache}`);
   const opShort =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/short-circuit.html?${globalThis.cache}`);
   const divOp = createElement('div');
+  const divNew = createElement('div');
   const divAss = createElement('div');
   const divShort = createElement('div');
   divOp.innerHTML = await opFunc;
   divAss.innerHTML = await opAss;
+  divNew.innerHTML = await opNew;
   divShort.innerHTML = await opShort;
   (await waitSelect('section[aria-labelledby="optional_chaining_with_function_calls"]')).appendChild(divOp);
+  (await waitSelect('section[aria-labelledby="invalid_optional_chaining"]')).appendChild(divNew);
   (await waitSelect('section[aria-labelledby="invalid_optional_chaining"]')).appendChild(divAss);
   (await waitSelect('section[aria-labelledby="short-circuiting"]')).appendChild(divShort);
   
