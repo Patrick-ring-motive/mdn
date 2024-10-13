03e4b7f49bc7c7036db57077cc6dca47729f1011
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-13 12:24:23 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/global_objects/promise/allsettled/fry.js
+++ files/en-us/web/javascript/reference/global_objects/promise/allsettled/fry.js
@@ -1,5 +1,24 @@
 void async function PromiseAllSettled(){
+  try{
+    globalThis.declare??await import(`https://unpkg.com/javaxscript/framework.js?${globalThis.cache}`);
+  }catch{
+    await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
+  }
+
     
-    //import example2
+  const code1 = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/global_objects/promise/allsettled/example2.html?${globalThis.cache}`);
+  (await waitExists('section[aria-labelledby="examples"]')).appendChild(assign(createElement('div'),{
+    innerHTML : (await code1)
+  }));
+
+
     
+  try {
+      Prism?.highlightAll?.();
+  } catch {
+      const prism = createElement("script");
+      prism.src = "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js";
+      body().appendChild(prism);
+  }
+
 }?.();
