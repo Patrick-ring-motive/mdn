3e040debbbdb82b7864e97ca70188bfdf11d6b51
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-14 15:36:33 -0500

    Update fry.js

--- files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
+++ files/en-us/web/javascript/reference/operators/optional_chaining/fry.js
@@ -4,23 +4,27 @@ void async function OptionalChaining(){
   }catch{
     await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
   }
-  const opFunc = zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-functions.html?${globalThis.cache}`);
-  const opNew =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-new.html?${globalThis.cache}`);
-  const opAss =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/optional-assignments.html?${globalThis.cache}`);
-  const opShort =  zfetchText(`https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/short-circuit.html?${globalThis.cache}`);
+  const dir = (loc) => `https://git-tdn.typescripts.org/Patrick-ring-motive/mdn/main/files/en-us/web/javascript/reference/operators/optional_chaining/${loc}.html?${globalThis.cache}`;
+  const opFunc = zfetchText(dir('optional-functions'));
+  const opNew =  zfetchText(dir('optional-new'));
+  const opAss =  zfetchText(dir('optional-assignments'));
+  const opShort =  zfetchText('short-circuit');
+  const opUndeclare =  zfetchText('undeclared');
   const divOp = createElement('div');
   const divNew = createElement('div');
   const divAss = createElement('div');
   const divShort = createElement('div');
+  const divUndeclare = createElement('div');
   divOp.innerHTML = await opFunc;
   divAss.innerHTML = await opAss;
   divNew.innerHTML = (await opNew).replace('<style>','');
   divShort.innerHTML = await opShort;
+  divUndeclare.innerHTML = await opUndeclare;
   (await waitSelect('section[aria-labelledby="optional_chaining_with_function_calls"]')).appendChild(divOp);
   (await waitSelect('section[aria-labelledby="invalid_optional_chaining"]')).appendChild(divNew);
   (await waitSelect('section[aria-labelledby="invalid_optional_chaining"] div.code-example')).after(divAss);
   (await waitSelect('section[aria-labelledby="short-circuiting"]')).appendChild(divShort);
-  
+  (await waitSelect('section[aria-labelledby="optional_chaining_with_function_calls"]')).before(divUndeclare);
   
   /*await Promise.all([import('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify.js'),
                      import('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.15.1/beautify-css.js'),
