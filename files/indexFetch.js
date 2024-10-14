a9506b16376da211e145fe9df59f875cc8a7e11f
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-14 12:15:46 -0500

    Update indexFetch.js

--- files/indexFetch.js
+++ files/indexFetch.js
@@ -11,17 +11,21 @@ const objDefProp=(obj, prop, def) => objDoProp (obj, prop, def, false, true);
 if(!globalThis['&fetch']){
   objDefProp(globalThis,"&fetch",fetch);
   globalThis.fetch = function fetch(url){
-    if(~String(arguments[0]).search(/index\.json($|\?.*|#.*)/i)){
-      console.log('redirecting index.json');
-      url = new URL(`${document.querySelector('link[hreflang="en"]')?.getAttribute?.('href')}/index.json`);
-      url.host = location.host;
-      arguments[0] = url;
-    }else if(String(arguments[0]).split('/')[2]=='developer.mozilla.org'){
-      console.log('redirecting mozilla fetch');
-      url = String(arguments[0]).split('/');
-      url[2] = location.host;
-      arguments[0] = url.join('/');
-    }
-    return globalThis['&fetch'].call(this,...arguments);
+    try{
+      if(~String(arguments[0]).search(/index\.json($|\?.*|#.*)/i)){
+        console.log('redirecting index.json');
+        url = new URL(`${document.querySelector('link[hreflang="en"]')?.getAttribute?.('href')}/index.json`);
+        url.host = location.host;
+        arguments[0] = url;
+      }else if(String(arguments[0]).split('/')[2]=='developer.mozilla.org'){
+        console.log('redirecting mozilla fetch');
+        url = String(arguments[0]).split('/');
+        url[2] = location.host;
+        arguments[0] = url.join('/');
+      }
+      return globalThis['&fetch'].call(this,...arguments);
+    }catch(e){
+      return new Response(e.message+'\n'+e.stack,{status:500,headers:{"Content-Type":"text/html","Access-Control-Allow-Origin":"*"}});
+   }
   }
 }
