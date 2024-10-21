b0acf3086f62712ed541607e77f30737b11393c3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-21 14:43:35 -0500

    Update fry.js

--- files/en-us/fry.js
+++ files/en-us/fry.js
@@ -15,10 +15,10 @@ void async function FrontPage(){
     await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
   }
 
-  select('.featured-articles>h2').after(Object.assign(createElement('div'),{
+  (await waitSelect('.featured-articles>h2')).after(Object.assign(createElement('div'),{
     className : "tile-container",
     innerHTML : `
-<div class="article-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a><h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Stuff</a></h3><p>Optional Chaining</p></div>
+<div class="article-tile"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a><h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Stuff</a></h3><p>Optional Chaining</p></div>
 `
   }));
 
