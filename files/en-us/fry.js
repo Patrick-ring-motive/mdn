738aac489e201497dc29597ef2de078a19584cab
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-21 14:34:38 -0500

    Update fry.js

--- files/en-us/fry.js
+++ files/en-us/fry.js
@@ -18,7 +18,7 @@ void async function FrontPage(){
   select('.featured-articles>h2').after(Object.assign(createElement('div'),{
     className : "title-container",
     innerHTML : `
-<div class="article-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a><h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">TDN partners with Scrimba to enhance web development learning</a></h3><p>We have chosen Scrimba as a course partner for the TDN Curriculum. This blog post explores what the partnership means practically, and how we will provide an even better web education experience together.</p></div>
+<div class="article-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a><h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Stuff</a></h3><p>Optional Chaining</p></div>
 `
   }));
 
