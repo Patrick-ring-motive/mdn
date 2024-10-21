e43ec1ed01fe00001da8bec27feb5ec744f98376
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-21 15:13:34 -0500

    Update fry.js

--- files/en-us/fry.js
+++ files/en-us/fry.js
@@ -15,13 +15,21 @@ void async function FrontPage(){
     await import(`https://git-tdn.typescripts.org/Patrick-ring-motive/framework/main/framework.js?${globalThis.cache}`);
   }
 
-  (await waitSelect('.featured-articles>h2')).after(Object.assign(createElement('div'),{
-    style : "width: 100%;  width: -webkit-fill-available; ",
-    innerHTML : `<div class="article-tile">
-    <a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a>
-    <h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Optional Chaining Reference</a></h3>
-    <p>Take a deeper dive into one of JavaScript's best features</p>
-    </div>`
-  }));
+  while(true){
+    if(select('dive[tdn-feature]')){
+      await sleep(100);
+      await nextIdle();
+    }else{
+      (await waitSelect('.featured-articles>h2')).after(Object.assign(createElement('div'),{
+        "tdn-feature" : true,
+        style : "width: 100%;  width: -webkit-fill-available; ",
+        innerHTML : `<div class="article-tile">
+        <a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a>
+        <h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Optional Chaining Reference</a></h3>
+        <p>Take a deeper dive into one of JavaScript's best features</p>
+        </div>`
+      }));
+    }
+  }
 
 }?.()
