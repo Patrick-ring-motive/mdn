03564d64851c0fb73d893bec61accf3075bac5c2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-21 14:37:33 -0500

    Update fry.js

--- files/en-us/fry.js
+++ files/en-us/fry.js
@@ -16,7 +16,7 @@ void async function FrontPage(){
   }
 
   select('.featured-articles>h2').after(Object.assign(createElement('div'),{
-    className : "title-container",
+    className : "tile-container",
     innerHTML : `
 <div class="article-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" class="tile-tag" data-glean="homepage: article_tag 1">Optional Chaining</a><h3 class="tile-title"><a href="https://developer.typescripts.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?cache=false" data-glean="homepage: article 1">Stuff</a></h3><p>Optional Chaining</p></div>
 `
