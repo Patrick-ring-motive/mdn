f8d3897df02f53cfa3fefc86e72270b970ca9619
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-10-28 21:25:10 -0500

    Update jookers.jsx

--- jookers.jsx
+++ jookers.jsx
@@ -1,8 +1,1 @@
-<script>
-declare(()=>{
-   queryApplyAll(<style>[class*="language-"]:not([class*="language-javascript"i],[class*="language-js"i])</style><script>,el=>{
-      el.className = 'language-js';
-      doPrism();
-   });
-});
-</script>
+ 
